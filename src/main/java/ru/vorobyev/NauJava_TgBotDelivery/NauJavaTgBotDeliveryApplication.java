5407889a7294743e7338f8b4d452403a57af4427
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2025-10-12 13:25:11 +0400

    Init

---
+++ src/main/java/ru/vorobyev/NauJava_TgBotDelivery/NauJavaTgBotDeliveryApplication.java
@@ -0,0 +1,13 @@
+package ru.vorobyev.NauJava_TgBotDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauJavaTgBotDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauJavaTgBotDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vorobyev/NauJava_TgBotDelivery/NauJavaTgBotDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vorobyev.NauJava_TgBotDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaTgBotDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
